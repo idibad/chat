a763938ea7b2746db23b9205359a518878cb05f2
idibad <83090441+idibad@users.noreply.github.com>
2026-02-04 01:45:13 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getDatabase, ref, push, onValue, remove, update, get } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
-import { getStorage, ref as sRef, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
 
 // ---------- Firebase Config ----------
 // // Firebase config
